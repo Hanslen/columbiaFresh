58e2a11260b70978155435ea3b5e571cbfad7521
Hanslen <chenjiaheyeah@gmail.com>
2018-01-28 17:23:20 -0500

    Fixed bugs

--- frontEnd/src/App.js
+++ frontEnd/src/App.js
@@ -4,12 +4,14 @@ import './App.css';
 import {Route, Switch, Redirect} from 'react-router-dom';
 import Layout from './hoc/Layout/Layout';
 import HomePage from './components/HomePage/HomePage';
+import Search from './components/Search/Search';
 
 class App extends Component {
   render() {
     let routes = (
       <Switch>
         <Route path="/" exact component={HomePage} />
+        <Route path="/search" exact component={Search}/>
       </Switch>
     );
     return (
---
+++ frontEnd/src/components/Search/Search.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import classes from './Search.css';
+class search extends Component{
+    render(){
+        return (
+            <div className={classes.test}>
+                _(:3=)_Something<br/>
+                _(:3=)_Something<br/>
+                _(:3=)_Something<br/>
+                _(:3=)_Something<br/>
+                _(:3=)_Something<br/>
+            </div>
+        );
+    }
+}
+export default search;
