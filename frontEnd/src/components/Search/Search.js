7bd20a33397507266aa095d96113dcfbdaafe655
Sylvanus <sylvanuszhy@gmail.com>
2018-01-29 16:50:36 -0500

    search layout

--- frontEnd/src/components/Search/Search.js
+++ frontEnd/src/components/Search/Search.js
@@ -1,14 +1,37 @@
-import React, {Component} from 'react';
+import React from 'react';
+import { Link } from 'react-router-dom'
 import classes from './Search.css';
-class search extends Component{
+import Sidebar from '../Sidebar/Sidebar';
+import SearchRes from '../SearchRes/SearchRes';
+import Reservation from '../Reservation/Reservation';
+
+class search extends React.Component{
     render(){
         return (
-            <div className={classes.test}>
-                _(:3=)_Something<br/>
-                _(:3=)_Something<br/>
-                _(:3=)_Something<br/>
-                _(:3=)_Something<br/>
-                _(:3=)_Something<br/>
+            <div className="container">
+                <div className="row standard-blank">
+                    <div className="col-2"></div>
+                    <div className="col-6">
+                        <div className="input-group">
+                            <input type="text" className="form-control" />
+                            <div className="input-group-append">
+                                <Link to="/searchMenu"><button type="button" className="btn btn-primary">Search</button></Link>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-4"></div>
+                </div>
+                <div className="row">
+                    <div className="col-2">
+                        <Sidebar />
+                    </div>
+                    <div className="col-6">
+                        <SearchRes />
+                    </div>
+                    <div className="col-4">
+                        <Reservation />
+                    </div>
+                </div>
             </div>
         );
     }
