5f5fa01efee96869729b0c50fc466423b8cef634
Hanslen <chenjiaheyeah@gmail.com>
2018-02-21 18:42:54 -0500

    Shopping cart

--- frontEnd/src/components/Account/ShoppingCart/ShoppingCart.js
+++ frontEnd/src/components/Account/ShoppingCart/ShoppingCart.js
@@ -1,10 +1,39 @@
 import React, { Component } from 'react';
 import MyHeader from '../MyHeader/MyHeader';
+import classes from './ShoppingCart.css';
 class shoppingcart extends Component{
+    state = {
+        items: [{
+            name: "Quaker Instant Oatmeal Variety Pack",
+            quality: "3",
+            src: "/static/img/order.jpg",
+            price: "123"
+        }]
+    }
     render(){
         return (
-            <div className="tab-pane fade" id="nav-shoppingCart" role="tabpanel" aria-labelledby="nav-shoppingCart-tab">
-            ShoppingCart
+            <div className="tab-pane fade show active" id="nav-shoppingCart" role="tabpanel" aria-labelledby="nav-shoppingCart-tab">
+                <div className={classes.shoppingCart}>
+                    <div className="row">
+                        <div className="col-md-8">Items</div>
+                        <div className="col-md-2">Price</div>
+                        <div className="col-md-2">Quantity</div>
+                    </div>
+                    <hr/>
+                    <div className={classes.item}>
+                        <div className="row">
+                            <div className="col-md-8">
+                                <img src={this.state.items[0].src}/>
+                                <a href="#">{this.state.items[0].name}</a>
+                            </div>
+                            <div className="col-md-2">
+                                <div className={classes.vertical}
+                                <p>${this.state.items[0].price}</p>
+                            </div>
+                            <div className="col-md-2"><p>{this.state.items[0].quality}</p></div>
+                        </div>
+                    </div>
+                </div>
             </div>    
         );
     }
