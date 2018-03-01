e69c86b56067622a47228591a58b721d41dcb8ab
EllaDing <yd2419@columbia.edu>
2018-03-01 12:55:06 -0500

    test dict

--- backEnd/tests/test_app.py
+++ backEnd/tests/test_app.py
@@ -1,6 +1,7 @@
 import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir, 'app')))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
 print (sys.path)
 
 from app import app
