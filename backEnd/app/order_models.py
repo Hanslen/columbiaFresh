f11e694458c112e5670b4390b4f5f576d8ba291e
EllaDing <yd2419@columbia.edu>
2018-04-07 11:53:23 -0400

    aaaa

--- backEnd/app/order_models.py
+++ backEnd/app/order_models.py
@@ -16,8 +16,3 @@ class OrderContainItems(db.Model):
     __tablename__ = 'order_contain_items'
     oid = db.Column(db.Integer, db.ForeignKey('order.uid'), nullable=False, primary_key=True)
     iid = db.Column(db.Integer, db.ForeignKey('ingredient.uid'), nullable=False, primary_key=True, index= True)
-
-class Cart(db.Model):
-    __tablename__ = 'cart'
-    cart_id = db.Column(db.Integer, nullable=False, primary_key=True)
-    uid = db.Column(db.Integer, db.ForeignKey('customer.uid'), nullable=False)
