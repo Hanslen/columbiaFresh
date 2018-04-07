4251ec3c40c11ca872dcbd8d2b91c3f6cac70af0
cnc12321 <david.ningchaocai@gmail.com>
2018-04-07 13:30:23 -0400

    adding view all orders function

--- backEnd/app/order_models.py
+++ backEnd/app/order_models.py
@@ -23,10 +23,21 @@ class Order(db.Model):
 
 
 class OrderContainItems(db.Model):
-    __tablename__ = 'order_contain_items'
+    __tablename__ = 'order_contain_recipes'
     oid = db.Column(db.Integer, db.ForeignKey('order.uid'), nullable=False, primary_key=True)
+    rid = db.Column(db.Integer, db.ForeignKey('recipe.uid'), nullable=False, primary_key=True)
+    quantity = db.Column(db.Integer, nullable=False)
 
-<<<<<<< HEAD
+    @staticmethod
+    def getOrderRecipe(oid):
+        temp = OrderContainItems.query.filter(OrderContainItems.oid == oid).all()
+        if temp is None:
+            print("The order does not exist!")
+            return None
+        else:
+            return temp
+
+
 class CartContainsRecipes(db.Model):
     __tablename__ = "cart_contains_recipe"
     uid = db.Column(db.Integer, db.ForeignKey('customer.uid'), nullable=False, primary_key=True)
@@ -38,6 +49,3 @@ class CartContainsRecipes(db.Model):
 #     __tablename__ = 'cart'
 #     cart_id = db.Column(db.Integer, nullable=False, primary_key=True)
 #     uid = db.Column(db.Integer, db.ForeignKey('customer.uid'), nullable=False)
-=======
-    iid = db.Column(db.Integer, db.ForeignKey('ingredient.uid'), nullable=False, primary_key=True, index= True)
->>>>>>> ebb83f0ff92ec21177272745763528d6f822d00e
