922a6edf3984902a7d60d60ed397bde986c0bad7
cnc12321 <david.ningchaocai@gmail.com>
2018-04-14 16:53:37 -0400

    wo yao feng le

--- backEnd/app/cart_models.py
+++ backEnd/app/cart_models.py
@@ -6,6 +6,62 @@ class Cart(db.Model):
     rid = db.Column(db.Integer, db.ForeignKey('recipe.rid'), primary_key=True)
     quantity = db.Column(db.Integer, nullable=False)
 
+    @staticmethod
+    def GetCartContent(uid, rid):
+        temp = Cart.query.filter(Cart.uid == uid). \
+            filter(Cart.rid == rid).first()
+        if temp is None:
+            return None
+        else:
+            return temp
+
+    @staticmethod
+    def CheckIfCartItemExists(uid, rid):
+        temp = Cart.GetCartContent(uid, rid)
+        if temp is not None:
+            return True, temp.quantity
+        else:
+            return False, 0
+
+    @staticmethod
+    def ModifyRecordContent(uid, rid, quantity, string):
+        temp = Cart.GetCartContent(uid, rid)
+        if temp is None:
+            return False
+        else:
+            if temp.quantity != quantity:
+                return False
+            else:
+                prev_quantity = temp.quantity
+                if string == "add":
+                    cur_quantity = prev_quantity + 1
+                elif string == "deduct":
+                    cur_quantity = prev_quantity - 1
+                else:
+                    return False
+                temp.quantity = cur_quantity
+                db.session.commit()
+                test_temp = Cart.GetCartContent(uid, rid)
+                if test_temp is None:
+                    if string == "add" and test_temp.quantity == prev_quantity + 1:
+                        return True
+                    elif string == "deduct" and test_temp.quantity == prev_quantity - 1:
+                        return True
+                    else:
+                        return False
+                else:
+                    return False
+
+    @staticmethod
+    def AddRecord(uid, rid, quantity):
+        addRecord = Cart(uid, rid, quantity)
+        db.session.add(addRecord)
+        temp = Cart.GetCartContent(uid, rid)
+        if temp is not None and temp.quantity == quantity:
+            return True
+        else:
+            return False
+
     def __init__(self, uid, rid, quantity):
         self.uid = uid
         self.rid = rid
--- backEnd/app/shopping/cart/views.py
+++ backEnd/app/shopping/cart/views.py
@@ -1,5 +1,6 @@
 from app import app
 from ...auth import check_token
+from ...cart_models import Cart
 
 @app.route('/addToCart', methods=['POST'])
 @check_token
@@ -7,13 +8,34 @@ def AddRecipeToCart(customer, content):
     try:
         uid = str(customer.uid)
         rid = content['rid']
-        quantity = content['quantity']
 
-        json = {
-            'state' : 'success',
-            'message' : 'this is a message.'
-        }
-        return (json, True)
+        valid, quantity = Cart.CheckIfCartItemExists(uid, rid)
+        if valid:
+            if Cart.ModifyRecordContent(uid, rid, quantity, "add"):
+                json = {
+                    'state': 'success',
+                    'message': 'Modified the record quantity...'
+                }
+                return (json, True)
+            else:
+                json = {
+                    'state': 'fail',
+                    'message': 'Internal Error...'
+                }
+                return (json, False)
+        else:
+            if Cart.AddRecord(uid, rid, 1):
+                json = {
+                    'state': 'success',
+                    'message': 'Added a record with quantity 1...'
+                }
+                return (json, True)
+            else:
+                json = {
+                    'state': 'fail',
+                    'message': 'Internal Error...'
+                }
+                return (json, False)
     except Exception as e:
         return (str(e), False)
 
